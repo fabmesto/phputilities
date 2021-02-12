409cbcc35f088fe847cc8e55e7fc8f856ac2d94a
Fab MacBook Pro <fabrizio.mesto@telnetsrl.com>
2021-02-13 00:42:26 +0100

    Update functions.php

--- src/fab/functions.php
+++ src/fab/functions.php
@@ -1,9 +1,34 @@
-<?php 
+<?php
+
 namespace fab;
 
-class functions{
+class functions
+{
 
-    public static function test(){
-        return 'ok';
+    public static function params_from_get($params)
+    {
+        //self::print_r($params);
+        //self::print_r($_GET);
+        $ret = array();
+        foreach ($params as $key => $default_value) {
+            $ret[$key] = $default_value;
+            if (isset($_GET[$key])) {
+                $ret[$key] = $_GET[$key];
+            }
+        }
+        return $ret;
+    }
+
+    public static function print_r($array)
+    {
+        $text = '<pre>';
+        $text .= print_r($array, true);
+        $text .= '</pre>';
+        return $text;
+    }
+
+    public static function test()
+    {
+        return 'test ok';
     }
 }
