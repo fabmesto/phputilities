eae3f49b6597612b4eb39e7934a33cc9ab02da63
Fab MacBook Pro <fabrizio.mesto@telnetsrl.com>
2021-02-13 08:20:32 +0100

    bootstrap

---
+++ src/bootstrap.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace fab;
+
+class bootstrap
+{
+    public static function html_input_edit($label, $input, $value, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label>' . $label . '</label>';
+        $html .= '<input type="text" class="form-control ' . $classes . '" name="' . $input . '" value="' . $value . '" placeholder="' . sanitize_html_class(strip_tags($label)) . '" ' . $attributes . '>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    public static function html_number_edit($label, $input, $value, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label>' . $label . '</label>';
+        $html .= '<input type="number" class="form-control ' . $classes . '" name="' . $input . '" value="' . $value . '" placeholder="' . sanitize_html_class(strip_tags($label)) . '" ' . $attributes . '>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    public static function html_textarea_edit($label, $input, $value, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label>' . $label . '</label>';
+        $html .= '<textarea class="form-control ' . $classes . '" name="' . $input . '" placeholder="' . sanitize_html_class(strip_tags($label)) . '" ' . $attributes . '>' . $value . '</textarea>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    public static function html_checkboxes_edit($label, $input, $values = array(), $sels = array(), $classes = "")
+    {
+        $html = '';
+        if (is_array($values) && count($values) > 0) {
+            $html .= '<div class="form-group">';
+            foreach ($values as $value => $text) {
+                $html .= '
+                    <input type="hidden" name="' . $input . '[' . $value . ']" value="0" />
+                    <div class="checkbox ' . $classes . '">
+                        <label>
+                            <input type="checkbox" name="' . $input . '[' . $value . ']" value="' . $value . '" ' . (in_array($value, $sels) ? 'checked' : '') . '> ' . $text . '
+                        </label>
+                    </div>';
+            }
+            $html .= '</div>';
+        }
+        return $html;
+    }
+
+    public static function html_radio_edit($label, $input, $values, $sel, $classes = "")
+    {
+        return self::_html_radio_edit($label, $input, $values, $sel, 'radio ' . $classes);
+    }
+
+    public static function html_radio_inline_edit($label, $input, $values, $sel, $classes = "")
+    {
+        return self::_html_radio_edit($label, $input, $values, $sel, 'radio-inline ' . $classes);
+    }
+
+    private static function _html_radio_edit($label, $input, $values, $sel, $classes = "")
+    {
+        $html = '';
+        if (is_array($values) && count($values) > 0) {
+            $html .= '<div class="form-check form-group">';
+            $html .= '<label>' . $label . '</label>';
+            foreach ($values as $value => $text) {
+                $html .= '<div class="' . $classes . '">';
+                $html .= '<label class="form-check-label"><input class="form-check-input" type="radio" name="' . $input . '" value="' . $value . '" ' . (strval($sel) === strval($value) ? 'checked' : '') . '>' . $text . '</label>';
+                $html .= '</div>';
+            }
+            $html .= '</div>';
+        }
+        return $html;
+    }
+
+    public static function html_radio_edit4($label, $input, $values, $sel, $classes = "",  $id_prefix = "radio_")
+    {
+        $html = '';
+        if (is_array($values) && count($values) > 0) {
+            $html .= '<div class="form-group-radio">';
+            if ($label != '') $html .= '<label>' . $label . '</label>';
+            foreach ($values as $value => $text) {
+                $html .= '<div class="form-check ' . $classes . '">';
+                $html .= '<input class="form-check-input" id="' . $id_prefix . $value . '" type="radio" name="' . $input . '" value="' . $value . '" ' . (strval($sel) === strval($value) ? 'checked' : '') . '>';
+                $html .= '<label class="form-check-label" for="' . $id_prefix . $value . '">' . $text . '</label>';
+                $html .= '</div>';
+            }
+            $html .= '</div>';
+        }
+        return $html;
+    }
+
+    public static function html_select_edit($label, $input, $options, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label>' . $label . '</label>';
+        $html .= '<select class="form-control ' . $classes . '" name="' . $input . '" ' . $attributes . '>';
+        $html .= $options;
+        $html .= '</select>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    public static function html_input_search($label, $input, $value, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label class="sr-only">' . $label . '</label>';
+        $html .= '<input type="text" class="form-control ' . $classes . '" name="' . $input . '" value="' . $value . '" placeholder="' . sanitize_html_class(strip_tags($label)) . '" ' . $attributes . '>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    public static function html_select_search($label, $input, $options, $classes = "", $attributes = "")
+    {
+        $html = '';
+        $html .= '<div class="form-group">';
+        $html .= '<label class="sr-only">' . $label . '</label>';
+        $html .= '<select class="form-control ' . $classes . '" name="' . $input . '" ' . $attributes . '>';
+        $html .= $options;
+        $html .= '</select>';
+        $html .= '</div>';
+        return $html;
+    }
+}
