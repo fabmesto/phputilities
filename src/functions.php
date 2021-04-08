578fa20d1a15dcb3fa142e85c0d07dda787e586f
Fab MacBook Pro <fabrizio.mesto@telnetsrl.com>
2021-04-08 13:30:33 +0200

    params_from_post

--- src/functions.php
+++ src/functions.php
@@ -6,13 +6,23 @@ class functions
 {
     public static function params_from_get($params)
     {
-        //self::print_r($params);
-        //self::print_r($_GET);
         $ret = array();
         foreach ($params as $key => $default_value) {
             $ret[$key] = $default_value;
             if (isset($_GET[$key])) {
                 $ret[$key] = $_GET[$key];
+            }
+        }
+        return $ret;
+    }
+
+    public static function params_from_post($params)
+    {
+        $ret = array();
+        foreach ($params as $key => $default_value) {
+            $ret[$key] = $default_value;
+            if (isset($_POST[$key])) {
+                $ret[$key] = $_POST[$key];
             }
         }
         return $ret;
---
+++ tests/functionsTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+use \PHPUnit\Framework\TestCase;
+use \fab\functions;
+
+final class functionsTest extends TestCase
+{
+
+    public function testCanBeCreated(): void
+    {
+        $this->assertInstanceOf(
+            functions::class,
+            new functions
+        );
+    }
+    /*
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        date::is_zero_date('10-10-2020');
+    }
+    */
+    public function test_params_from_get(): void
+    {
+        $_GET['key1'] = 'a';
+        $_GET['key2'] = 'b';
+        $params = ['key1' => 1, 'key2' => 2, 'key3' => 3];
+        $this->assertEquals(
+            ['key1' => 'a', 'key2' => 'b', 'key3' => 3],
+            functions::params_from_get($params)
+        );
+        unset($_GET);
+    }
+
+    public function test_params_from_post(): void
+    {
+        $_POST['key1'] = 'aa';
+        $_POST['key2'] = 'bb';
+        $params = ['key1' => 1, 'key2' => 2, 'key3' => 3];
+        $this->assertEquals(
+            ['key1' => 'aa', 'key2' => 'bb', 'key3' => 3],
+            functions::params_from_post($params)
+        );
+        unset($_POST);
+    }
+
+    public function test_arraymulti_to_keys_values(): void
+    {
+        $params = [
+            ['key' => 0, 'text' => 'testo 0', 'other' => 'other'],
+            ['key' => 1, 'text' => 'testo 1', 'other' => 'other']
+        ];
+        $expected = [0 => 'testo 0', 1 => 'testo 1'];
+
+        $result = functions::arraymulti_to_keys_values($params, 'key', 'text');
+
+        $this->assertEquals(
+            $expected,
+            $result
+        );
+    }
+
+    public function test_arraymulti_to_keys_values_group(): void
+    {
+        $params = [
+            [
+                'group' => [
+                    ['key' => 0, 'text' => 'testo 0', 'other' => 'other'],
+                    ['key' => 1, 'text' => 'testo 1', 'other' => 'other']
+                ],
+                'g_name' => 'gruppo 1',
+            ],
+            [
+                'group' => [
+                    ['key' => 2, 'text' => 'testo 2', 'other' => 'other'],
+                    ['key' => 3, 'text' => 'testo 3', 'other' => 'other']
+                ],
+                'g_name' => 'gruppo 2',
+            ]
+        ];
+        $expected = [
+            'gruppo 1' => [
+
+                0 => 'testo 0',
+                1 => 'testo 1',
+            ],
+            'gruppo 2' => [
+
+                2 => 'testo 2',
+                3 => 'testo 3',
+            ],
+        ];
+
+        $result = functions::arraymulti_to_keys_values_group($params, 'group', 'g_name', 'key', 'text');
+
+        $this->assertEquals(
+            $expected,
+            $result
+        );
+    }
+
+    public function test_arraymulti_to_keys()
+    {
+        $params = [
+            ['key' => 1],
+            ['key' => 2],
+            ['key' => 3]
+        ];
+        $expected = [1, 2, 3];
+        $result = functions::arraymulti_to_keys($params, 'key');
+
+        $this->assertEquals(
+            $expected,
+            $result
+        );
+    }
+}
